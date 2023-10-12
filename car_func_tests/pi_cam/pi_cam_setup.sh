0054a21ce0c746a37d1696e09b6a7f434d0d40c0
adamsadek <adam.sadekk@gmail.com>
2023-10-12 18:18:14 +0100

    Additions to script that will setup the camera before testing

--- car_func_tests/pi_cam/pi_cam_setup.sh
+++ car_func_tests/pi_cam/pi_cam_setup.sh
@@ -6,12 +6,26 @@ sudo apt install python3-pip python3-dev build-essential
 
 # for workon cv setup 
 sudo apt install virtualenv virtualenvwrapper
+
+# setting some env vars to make sure workon works
+export WORKON_HOME=~/.env
+VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+# note this path below might be different for you!
+source /home/adam/.local/bin/virtualenvwrapper.sh
+
+# creating a new python virtual environment and then activing it with workon for the py development
 mkvirtualenv cv
-# export WORKON_HOME=$HOME/.virtualenvs
-# VIRTUALENVWRAPPER_PYTHON='/usr/bin/python3'
-# # note this path below might be different for you!
-# source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 workon cv
 
-pip3 install "picamera[array]"
+# setting some env variables to satisfy the cv enviroment
+echo "start_x=1" | sudo tee -a /boot/firmware/config.txt
+echo "gpu_mem=128" | sudo tee -a /boot/firmware/config.txt
+
+# installing picamera packages
+sudo pip3 install picamera
+sudo pip3 install "picamera[array]"
+
+# giving my user camera permissions incase
+# Note that the UID will most likely be different for you
+sudo usermod -a -G video adam
 

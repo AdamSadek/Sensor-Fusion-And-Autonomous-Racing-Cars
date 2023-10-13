b33c8618fe6da46b3ec6fdc4baad5554508008c1
adamsadek <adam.sadekk@gmail.com>
2023-10-13 10:28:11 +0100

    Updated script to satisfy env for camera sensor

--- car_func_tests/pi_cam/pi_cam_setup.sh
+++ car_func_tests/pi_cam/pi_cam_setup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# some env setups that might be needed
+# installing some build essentials
 sudo apt update
 sudo apt install python3-pip python3-dev build-essential
+sudo pip3 install PyQt5
+sudo pip3 install opencv-python
 
 # for workon cv setup 
 sudo apt install virtualenv virtualenvwrapper
@@ -10,6 +12,7 @@ sudo apt install virtualenv virtualenvwrapper
 # setting some env vars to make sure workon works
 export WORKON_HOME=~/.env
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+
 # note this path below might be different for you!
 source /home/adam/.local/bin/virtualenvwrapper.sh
 
@@ -21,11 +24,8 @@ workon cv
 echo "start_x=1" | sudo tee -a /boot/firmware/config.txt
 echo "gpu_mem=128" | sudo tee -a /boot/firmware/config.txt
 
-# installing picamera packages
-sudo pip3 install picamera
-sudo pip3 install "picamera[array]"
-
 # giving my user camera permissions incase
-# Note that the UID will most likely be different for you
-sudo usermod -a -G video adam
+sudo usermod -a -G video $USER
+
+
 

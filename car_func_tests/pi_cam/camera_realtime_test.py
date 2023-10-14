5bc2d72a9b23619734039f4f1b8744218f40f153
adamsadek <adam.sadekk@gmail.com>
2023-10-14 15:26:37 +0100

    opencv set up with make build and live cam view test

---
+++ car_func_tests/pi_cam/camera_realtime_test.py
@@ -0,0 +1,25 @@
+"""
+Adam Sadek
+Real-time camera view test script
+
+This code displays live video from the camera and exits when stopped by the letter 'q'.
+"""
+import cv2
+
+# setting video capture, we use 0 as thats where the pi camera index
+video_capture = cv2.VideoCapture(0)
+
+while True:
+    # storing frame data in the frame variable
+    ret, frame = video_capture.read()
+
+    # displaying the frame
+    cv2.imshow("Pi-Cam View", frame)
+
+    # Press the 'q' key to exit the loop
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# close the video capture and windows when we're done
+video_capture.release()
+cv2.destroyAllWindows()
--- car_func_tests/pi_cam/camera_test.py
+++
@@ -1,5 +0,0 @@
-# import the necessary packages
-from picamera.array import PiRGBArray
-from picamera import PiCamera
-import time
-import cv2

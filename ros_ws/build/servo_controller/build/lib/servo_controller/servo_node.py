42806f44f38d08d299931326c1169d1a9a7ea93a
adamsadek <adam.sadekk@gmail.com>
2024-01-23 11:14:54 +0000

    Servo now moves through ROS

---
+++ ros_ws/build/servo_controller/build/lib/servo_controller/servo_node.py
@@ -0,0 +1,85 @@
+"""
+Adam Sadek
+
+This code tests a servo motor's steering by moving it between two angles (0 and 150 degrees) with a delay,
+and it returns the servo to a neutral position when it completes the test 10 times.
+"""
+
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+import RPi.GPIO as GPIO
+import time
+
+class ServoControlNode(Node):
+
+    def __init__(self):
+        # creating the node with the name servo_control_node
+        super().__init__('servo_control_node')
+        # setting up ROS2 publisher for servo control on the '/servo_movement' topic
+        self.servo_publisher = self.create_publisher(Float64, '/servo_movement', 10)
+
+        # setting up GPIO for servo control
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(11, GPIO.OUT)
+        # creating PWM instance for GPIO pin 11 with frequency of 50 Hz
+        self.servo = GPIO.PWM(11, 50)
+        self.servo.start(7.1)
+
+    # publishing servo angle on the '/servo_movement' topic
+    def update_servo_movement(self, angle):
+        msg = Float64()
+        msg.data = float(angle)
+        self.servo_publisher.publish(msg)
+
+        # calculating duty cycle on the provided angle and move the servo
+        common_scale_factor = 18.0
+        min_duty_cycle = 2.5
+        duty_cycle = min_duty_cycle + (angle / common_scale_factor)
+        self.servo.ChangeDutyCycle(duty_cycle)
+        time.sleep(0.5)
+
+    def servo_to_neutral(self):
+        # return servo to neutral position
+        self.servo.ChangeDutyCycle(7.1)
+        time.sleep(1)
+
+    def run_servo_test(self):
+        test_count = 0
+        while test_count != 10:
+            # moving servo to 0 degrees
+            self.update_servo_movement(0)
+            time.sleep(1)
+
+            # moving servo to 150 degrees
+            self.update_servo_movement(150)
+            time.sleep(1)
+
+            test_count += 1
+            print("Test #", test_count)
+
+        servo_to_neutral(self)
+
+    def cleanup(self):
+        self.servo.stop()
+        GPIO.cleanup()
+
+    def spin(self):
+        # run the servo test, cleanup, and stop the ROS2 node with some nice debugging
+        self.get_logger().info("Servo control node is running.")
+        self.run_servo_test()
+        self.cleanup()
+        self.get_logger().info("Servo control node is stopping.")
+        self.destroy_node()
+
+def main():
+    # starting ROS2
+    rclpy.init()
+    # creating and running the ServoControlNode
+    servo_node = ServoControlNode()
+    servo_node.spin()
+    # stopping ROS2
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- ros_ws/install/motor_controller/lib/python3.8/site-packages/motor_controller/motor_node.py
+++ ros_ws/install/motor_controller/lib/python3.8/site-packages/motor_controller/motor_node.py
@@ -1,96 +1,33 @@
-# """
-# Adam Sadek
+"""
+Adam Sadek
 
-# When creating these nodes, the ROS2 docs have proven to be very useful i.e 
-# """
+Motor node for controlling a motor's speed using PWM on a RaspberryPi. It publishes motor throttle values on the "/motor_throttle" topic.
+The script utilizes RPi.GPIO for GPIO control and std_msgs/Float64 messages in ROS2.
+"""
 
-# import rclpy
-# from rclpy.node import Node
-# import cv2
-# import RPi.GPIO as GPIO
-# from std_msgs.msg import Float32
-
-
-# class MotorNode(Node):
-
-#     # creating a ROS2 node and naming it 'motor_node'
-#     def __init__(self):
-#         # creating the node with the name motor_node
-#         Node.__init__(self, 'motor_node')
-
-#         # create publisher to send motor signals. Here, motor_control will be the topic that will have msgs sent to it.
-#         # Note that the topic will be created if it doesn't already exist.
-#         subscriber_q_size = 10
-#         self.motor_publisher = self.create_publisher(Float32, 'motor_control', subscriber_q_size)
-
-#         # Create a subscriber to receive control commands
-#         self.control_command_subscription = self.create_subscription(Float32, 'control_command', self.run_motor, subscriber_q_size)
-#         #self.control_command_subscription  # Prevent unused variable warning
-
-
-#         # here i'm setting the literal GPIO pin locations to be used
-#         GPIO.setmode(GPIO.BOARD)
-
-#         # pin number 13 is where i've connected the motor so I need to make it available for signals
-#         GPIO.setup(13,GPIO.OUT)
-
-#         # here I use Pulse Width Modulation to control the motor
-#         self.motor = GPIO.PWM(13, 50)
-
-#         # starting the car at a low speed
-#         self.motor.start(0)
-        
-
-#     def run_motor(self, msg):
-#         # getting the control commands. + values mean forward, - values mean reverse.
-#         set_speed = msg.data
-
-#         # Ensure the speed setpoint is within a valid range (e.g., -1.0 to 1.0)
-#         # speed_setpoint = max(-1.0, min(1.0, speed_setpoint))
-
-#         # Convert the speed setpoint to a motor control command (e.g., PWM signal)
-#         speed_to_cmd = 50 * speed_setpoint  # Adjust this based on your hardware interface
-
-#         # Publish the motor control command
-#         # self.motor_publisher.publish(speed_to_cmd)
-#         self.motor.ChangeDutyCycle(speed_to_cmd)
-
-#         motor_msg = Float32()
-#         motor_msg.data = motor_command
-#         self.motor_publisher.publish(motor_msg)
-
-
-# def main():
-#     # initilizing ROS2, no arguments for now
-#     rclpy.init(args=None)
-
-#     # here we're just creating an object of the class above "MotorNode" so we can call it later.
-#     motor_node = MotorNode()
-
-#     # this will keep the node running until we stop it
-#     rclpy.spin(motor_node)
-
-#     # telling ROS to stop and clean up
-#     rclpy.shutdown()
 import rclpy
 from std_msgs.msg import Float64
+from rclpy.node import Node
 import RPi.GPIO as GPIO
 import time
 
-class MotorControlNode:
+class MotorControlNode(Node):
 
     def __init__(self):
-        self.node = rclpy.create_node('motor_control_node')
+        # creating the ROS node
+        super().__init__('motor_control_node')
+        # this is the ROS publisher for motor control on the '/motor_throttle' topic
+        self.motor_publisher = self.create_publisher(Float64, '/motor_throttle', 10)
 
-        # Set up ROS 2 publisher for motor control
-        self.motor_pub = self.node.create_publisher(Float64, '/motor_throttle', 10)
-
-        # Set up GPIO
+        # setting up GPIO for motor control
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(13, GPIO.OUT)
+        # creating PWM instance for GPIO pin 13 with frequency 100 Hz (not working when on 50hz - need to investigate a bit)
         self.motor = GPIO.PWM(13, 100)
+        # starting PWM with initial duty cycle '0'
         self.motor.start(0)
 
+    # same as my test script
     def run_motor_test(self):
         test_count = 1
         while test_count != 3:
@@ -98,14 +35,14 @@ class MotorControlNode:
             min_speed = 5
             max_speed = 10
 
-            # Going from low to high speed
+            # going from low to high speed
             print("* Low to high speed *")
             for throttle in range(min_speed, max_speed):
                 self.publish_motor_throttle(throttle)
                 time.sleep(1)
                 print("Speed: ", throttle)
 
-            # Going from high to low speed
+            # going from high to low speed
             print("* High to low speed *")
             for throttle in range(max_speed, min_speed, -1):
                 self.publish_motor_throttle(throttle)
@@ -114,32 +51,39 @@ class MotorControlNode:
 
             test_count += 1
 
+    # publishing motor throttle value to the '/motor_throttle' topic
     def publish_motor_throttle(self, throttle):
         print("Publishing motor throttle:", throttle)
         msg = Float64()
         msg.data = float(throttle)
-        self.motor_pub.publish(msg)
+        self.motor_publisher.publish(msg)
+
+        # setting the PWM duty cycle to control the motor speed
         print("Changing duty cycle to:", throttle)
         self.motor.ChangeDutyCycle(throttle)
 
     def cleanup(self):
+        # stopping the motor and clean up GPIO
         self.motor.stop()
         GPIO.cleanup()
 
     def spin(self):
-        self.node.get_logger().info("Motor control node is running.")
+        # running the motor test, cleanup, and stop the ROS 2 node
+        # extra logging - logging important information with ROS2 loggers
+        self.get_logger().info("Motor control node is running.")
         self.run_motor_test()
         self.cleanup()
-        self.node.get_logger().info("Motor control node is stopping.")
-        self.node.destroy_node()
+        self.get_logger().info("Motor control node is stopping.")
+        self.destroy_node()
 
 def main():
+    # starting ROS
     rclpy.init()
+    # creating and running the MotorControlNode
     motor_node = MotorControlNode()
     motor_node.spin()
+    # stoppping ROS 2
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
-
-

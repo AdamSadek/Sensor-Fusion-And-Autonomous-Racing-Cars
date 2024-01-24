ff0251117124b4150c6a32f96bf52684e2f0155f
adamsadek <adam.sadekk@gmail.com>
2024-01-24 11:25:13 +0000

    Moving some files and tidying some directories

--- ros_ws/src/build/ldlidar_ros2/sdk/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adam/Sensor-Fusion-And-Autonomous-Racing-Cars/ros_ws/src/lidar_controller/sdk
-# Build directory: /home/adam/Sensor-Fusion-And-Autonomous-Racing-Cars/ros_ws/src/build/ldlidar_ros2/sdk
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
